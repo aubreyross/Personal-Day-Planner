7c6b37614e931ab65f1598a656144c3556fe75ab
Aubrey Ross <aubrey.ross04@gmail.com>
2021-09-10 23:13:03 -0500

    i added a file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Personal-Day-Planner
+
+## Description
+Created a personal day planner application that runs in the browser and is composed of HTML, CSS, JS, jQuery and Boostrap.  
+---
+
+## HTML
+• Utilized Bootstrap for day planner layout
+• Input (1) hour time blocks ranging from the hours of 9am to 5pm.
+
+## CSS
+• Used CSS provided. 
+
+## JS
+• Display date at top with moment.js
+• Time blocks are coordinated with colors to determine past/present/future
+• Input is saved to localStorage
+---
+
+## Resources
+https://jquery.com/
+https://getbootstrap.com/
+https://momentjs.com/docs/#/displaying/
+https://developer.mozilla.org/
+https://fontawesome.com/v5.15/icons/save
---
+++ script.js
@@ -0,0 +1,56 @@
+var timeDisplayEl = $('#currentDay');
+var saveBtnEl= $(".saveBtn")
+
+//displays the current time and date using moment.js//
+function displayTime() {
+    var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(currentTime);
+  }
+  
+setInterval(displayTime, 1000);
+
+//event listener for save button//
+$(document).ready(function () {
+    saveBtnEl.on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var rowHour = $(this).parent().attr("id");
+
+        localStorage.setItem(rowHour, text);
+    });
+
+    
+   //In HTML each row was given an ID that represented the hour. This function compares the schedule hour to the current hour to determine past, present, or future.
+    function colorBlock() {
+        var currentHour = moment().hour();
+
+
+        $(".time-block").each(function () {
+            var rowHour = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (rowHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (rowHour === currentHour) {
+                $(this).addClass("present");
+            }
+            else {
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    //localStorage//
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    colorBlock();
+})
+
